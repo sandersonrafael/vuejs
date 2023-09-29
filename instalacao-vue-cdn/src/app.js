6cff5add843adeaa3e1b32fd06a4295e209cb982
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-09-29 14:09:08 -0300

    data binding

--- instalacao-vue-cdn/src/app.js
+++ instalacao-vue-cdn/src/app.js
@@ -1,11 +1,25 @@
+// Instalação e uso
+// Vue.createApp(objetoComDados).mount("id_do_elemento")
+
 const MyApp = {
   data(){
     return {
       name: "Fulano",
-      age: 35
+      age: 35,
+      input_name: "",
+    }
+  },
+  /* data binding */
+  // Capacidade de mudar em tempo real um dado (por exemplo: através de inputs)
+  methods: {
+    submitForm(e) {
+      e.preventDefault();
+      this.name = this.input_name;
     }
   }
 };
 
 
 Vue.createApp(MyApp).mount("#app");
+
+
