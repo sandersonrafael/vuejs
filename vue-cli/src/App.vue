617b60927fbcd17748e5295cbcdb5c5731465321
= <=>
2023-09-29 17:23:31 -0300

    Componentes, dados e names de componentes

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ teste }}</h1>
+  <PrimeiroComponente />
+</template>
+
+<script lang="ts">
+import PrimeiroComponente from './components/PrimeiroComponente.vue';
+export default {
+  name: 'App',
+  components: {
+    PrimeiroComponente,
+  },
+  data() {
+    return {
+      teste: 'testado',
+    };
+  },
+};
+</script>
---
+++ vue-cli/src/components/PrimeiroComponente.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Olá vue!</h1>
+  <p>Meu nome é: {{ nome }}</p>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'PrimeiroComponente',
+  data() { // esses dados só podem ser utilizados dentro desse componente, não fora
+    return {
+      nome: 'Jaumzim',
+      profissao: 'Programador',
+    };
+  },
+};
+</script>
